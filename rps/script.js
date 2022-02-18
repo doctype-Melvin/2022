b2a2b457af147c9f872b34d6c0736d81acdb9045
doctype-Melvin <doctype.melvin@gmail.com>
2022-02-18 06:46:50 +0100

    round message now updates each round

--- rps/script.js
+++ rps/script.js
@@ -35,13 +35,16 @@ selections.forEach(button => {
 function playRound(player, cpu) {
     cpu = randomChoice();
     showHand(player, cpu);
+    alertWin(player, cpu)
     console.log(player.icon, cpu.icon)
 }
 
-//Show played hand
+//Show played hand and round outcome message
 const outcome = document.querySelector('#messages');
 const playerHand = document.createElement('div');
 playerHand.classList.add('player-hand');
+const message = document.createElement('div');
+message.classList.add('round-winner');
 const cpuHand = document.createElement('div');
 cpuHand.classList.add('cpu-hand');
 
@@ -50,5 +53,11 @@ function showHand(ply, cpu) {
     let computer = cpu.icon;
         playerHand.textContent = player;
         cpuHand.textContent = computer;
-        outcome.append(playerHand, cpuHand)
+        outcome.append(playerHand, message, cpuHand)
+}
+
+function alertWin(ply, cpu) {
+    return (ply.beats===cpu.name) ? message.textContent = `${ply.name} beats ${cpu.name}` :
+    (cpu.beats===ply.name) ? message.textContent = `${ply.name} beaten by ${cpu.name}` :
+    message.textContent = `It's a draw!`
 }
