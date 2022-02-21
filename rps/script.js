0b3f60476f9f56e3edefb5396687b465018834bf
doctype-Melvin <doctype.melvin@gmail.com>
2022-02-21 12:06:49 +0100

    New script including working counter, roundbased winner message

--- rps/script.js
+++ rps/script.js
@@ -16,72 +16,67 @@ const RPS = [
     }
 ]
 
-//Random computer choice
-function randomChoice(){
-    return RPS[Math.floor(Math.random()*choices.length)]
+//Random choice function
+function computerChoice(){
+    return RPS[Math.floor(Math.random()*RPS.length)]
 }
-//Player choice - Button starts round
+
+//Player choice function
 const choices = document.querySelectorAll('[data-selection]');
-    choices.forEach(button => button.addEventListener('click', () => {
-        const choiceName = button.dataset.selection;
-        const choice = RPS.find(choice => choice.name === choiceName);
-            playRound(choice);
+choices.forEach(button => button.addEventListener('click', () => {
+    let choiceName = button.dataset.selection;
+    let choice = RPS.find(choice => choice.name === choiceName);
+    playRound(choice);
 }));
 
-//Main play round function
-function playRound(choice){
-    const roboto = randomChoice();
-    const playerWins = whoWins(choice, roboto);
-    const robotoWins = whoWins(roboto, choice);
+//Play round function
+function playRound(choice) {
+    let roboto = computerChoice();
+    let playerWins = whoWins(choice, roboto);
+    let robotoWins = whoWins(roboto, choice);
+        if(playerWins) {
+        trackScore(player),
+        ply();
+            }else if(robotoWins) {
+            trackScore(cpu),
+            computer();
+                }else draw();
     showHands(choice, roboto);
-    
 }
 
-//Determin winner function
-function whoWins(choice, robotoChoice) {
-    return choice.beats === robotoChoice.name
+//Winner function
+function whoWins(winner, loser) {
+   return winner.beats === loser.name
 }
 
-//Show hands
-const outcome = document.querySelector('#messages');
-const message = document.createElement('div');
-message.classList.add('round-winner');
+//Track points
+const player = document.querySelector('[data-your-score]');
+const cpu = document.querySelector('[data-cpu-score]');
+
+function trackScore(score) {
+    score.innerText = parseInt(score.innerText)+1
+}
+
+//Round messages
+const board = document.querySelector('#messages');
+const current = document.createElement('div');
+current.classList.add('round-winner')
 const pHand = document.createElement('div');
 const cHand = document.createElement('div');
 
-function showHands(player, computer){
-    let ply = player.icon;
-        pHand.textContent = ply;
-    let cpu = computer.icon;
-        cHand.textContent = cpu;
-        message.textContent = '';
-            outcome.append(pHand, message, cHand)
+function showHands(player, cpu) {
+    pHand.innerText = player.icon;
+    cHand.innerText = cpu.icon;
+    //current.innerText = ''
+    board.append(pHand, current, cHand)
 }
-
-
-
-
-/*
-const display = document.querySelector('#messages');
-const info = document.createElement('div');
-info.classList.add('round-winner');
-const pHand = document.createElement('div');
-const cHand = document.createElement('div');
-
-function showHands(winner){
-    const winning = winner.name;
-        pHand.textContent = winner.icon;
-    const losing = winner.beats;
-    const loser = RPS.find(loser => loser.name === losing);
-        cHand.textContent = loser.icon
-    info.textContent = `${winning} beats ${losing}`
-    display.append(pHand, info, cHand)
+//Helper functions for message board
+function ply() {
+    current.innerText = 'Beats';
 }
-*/
-
-/*Things to add
-Transition from clicking choice to two fists engaging (round initiation)
-Fade from round initiation to outcome
-Keep track of outcome history
-Choosing game length (3 rounds, 5 rounds, 11 rounds)
-Optional: character choice*/
+function computer() {
+    current.innerText = 'Beaten by';
+}
+function draw() {
+    current.innerText = 'Draw'
+}
