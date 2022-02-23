32328d8382d5e8a8cf0a37594ccf9b4ff2adfae5
doctype-Melvin <doctype.melvin@gmail.com>
2022-02-23 08:38:18 +0100

    working code including color picker, eraser and rainbow mode

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -5,6 +5,7 @@ const gridContainer = document.querySelector('.grid-container');
 //Slider to change grid size
 const sliderValue = document.createElement('div');
 sliderValue.classList.add('sliderValue');
+sliderValue.textContent = '16 x 16'
 slider.append(sliderValue);
 
 slider.addEventListener('click', (e) => {
@@ -13,6 +14,48 @@ slider.addEventListener('click', (e) => {
     setSize(value)
 });
 
+//Add buttons to menu
+//Eraser
+const eraser = document.createElement('button');
+eraser.textContent = 'Clear';
+menu.append(eraser);
+eraser.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.style = 'background: white')
+})
+
+    //Rainbow mode
+    const rainbow = document.createElement('button');
+    rainbow.textContent = 'Rainbow';
+    menu.append(rainbow);
+    rainbow.addEventListener('click', () => {
+        const squares = document.querySelectorAll('.square');
+        squares.forEach(square => square.addEventListener('mouseover', () => {
+            let r = Math.random()*255;
+            let g = Math.random()*255;
+            let b = Math.random()*255;
+            square.style = `background: rgb(${r}, ${g}, ${b})`
+        }))
+    })
+
+//Function manipulates square style
+const palette = document.createElement('input');
+palette.setAttribute('type', 'color');
+menu.append(palette);
+
+function drawSquares(){
+    const squares = document.querySelectorAll('.square');
+    //On input the background is set to color choice
+palette.addEventListener('input', (e) => {
+    squares.forEach(square => square.addEventListener('mouseover', () => {
+        square.style.background = e.target.value;
+    }))
+})
+    //Default painting functionality
+squares.forEach(square => square.addEventListener('mouseover', () => {
+    square.style.background = 'black';
+}));
+};
 
 //Function initializes grid
 function initGrid(){
@@ -23,6 +66,7 @@ function initGrid(){
         square.classList.add('square');
         gridContainer.appendChild(square);   
     }
+    drawSquares()
 }
 initGrid()
 
@@ -45,10 +89,6 @@ function setSize(value){
         square.classList.add('square');
         gridContainer.append(square);
     }
+    drawSquares();
 }
 
-//Function manipulates square style
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener('mouseover', (e) => {
-        e.target.classList.remove('square');
-}));
