8e7e1f6cfc57ba823f1cc99fd702c954137b85ca
doctype-Melvin <doctype.melvin@gmail.com>
2022-02-24 06:16:57 +0100

    Object lookup contains operations

--- calculator/script.js
+++ calculator/script.js
@@ -2,42 +2,55 @@ const OPERATORS = [
     {
         operator: '+',
         name: 'add',
-        key: 171
+        key: 171,
+        calc: function(num1, num2) {
+            return num1 += num2
+        }
     },
     {
         operator: '-',
         name: 'subtract',
-        key: 173
+        key: 173,
+        calc: function(num1, num2) {
+            return num1-num2
+        }
     },
     {
-        operator: 'x',
+        operator: '*',
         name: 'multiply',
-        key: 171
+        key: 171,
+        calc: function(num1, num2) {
+            return num1*num2
+        }
     },
     {
         operator: '/',
         name: 'divide',
-        key: 55
+        key: 55,
+        calc: function(num1, num2) {
+            return num1/num2
+        }
     }
 ]
 
 function operate(operator, num1, num2) {
-    
+    let operation = OPERATORS.find(element => operator === element.operator)
+    return operation.calc(num1, num2)
 }
 
-function add(num1, num2) {
-    let result = num1 += num2;
-    return result
-}
+// function add(num1, num2) {
+//     let result = num1 += num2;
+//     return result
+// }
 
-function subtract(num1, num2) {
-    return num1-num2
-}
+// function subtract(num1, num2) {
+//     return num1-num2
+// }
 
-function multiply(num1, num2) {
-    return num1*num2
-}
+// function multiply(num1, num2) {
+//     return num1*num2
+// }
 
-function divide(num1, num2) {
-    return num1/num2
-}
+// function divide(num1, num2) {
+//     return num1/num2
+// }
