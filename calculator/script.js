19091760aee52389c5cf0375e91709396addbe06
doctype-Melvin <doctype.melvin@gmail.com>
2022-02-26 08:42:33 +0100

    wrote out some logic for review

--- calculator/script.js
+++ calculator/script.js
@@ -51,7 +51,7 @@ const clear = document.querySelector('.clear');
 const equals = document.querySelector('.equals');
 
 //Selector for numblock
-const num = document.querySelectorAll('.num');
+const numblock = document.querySelectorAll('.num');
 
 //Selector for display
 const display = document.querySelector('.display');
@@ -62,4 +62,21 @@ let num1 = null;
 let num2 = null;
 let displayValue = 0;
 let storage = [];
+let finalArray = [`${operator}, ${num1}, ${num2}`] //Pass this to operate()
+
+
+
+
+
+
+// 1. Single digits are the Input
+// 2. Input should go into an array 
+// 3. The operator marks the first number and the desired operation
+// 4. Num1 variable should be set to first number and operator variable to operator
+// 5. The digits of the second number go into the array
+// 6. Num2 variable should be set to second number
+// 7. Now all arguments are met
+// 8. On triggering equals or another operator, the operate function should be called and return the result 
+// 9. In case another operator has been clicked, the first result is now Num1 variable 
+// 10. Start from 4.
 
