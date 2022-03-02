10915317b660ea31cbdbf4568254065f574e439f
doctype-Melvin <doctype.melvin@gmail.com>
2022-03-02 10:35:14 +0100

    basic calculator functionality implemented

--- calculator/script.js
+++ calculator/script.js
@@ -26,13 +26,6 @@ const CALCULATIONS = [
     }
 ];
 
-// const calculator = {
-//     value: 0,
-//     firstNum: null,
-//     secondNum: false,
-//     operator: null
-// };
-
 function calculate(num1, num2, operator){
     let calculation = CALCULATIONS.find(el => operator === el.name);
     return calculation.calc(num1, num2)
@@ -41,23 +34,62 @@ function calculate(num1, num2, operator){
 const display = document.querySelector('.display');
 const numBlock = document.querySelectorAll('.num');
 const operators = document.querySelectorAll('.operator');
+const decimal = document.querySelector('.decimal');
+const equals = document.querySelector('#equals').textContent
 
-let numbers = [];
-let operator = []
+let displayValue = display.textContent
+let num1 = '';
+let num2 = '';
+let operator = '';
+
+//Numblock event listeners
 
 numBlock.forEach(button => button.addEventListener('click', (e) => {
-    display.append(e.target.textContent);    
-}))
+    let num = e.target.textContent;
+    appendNum(num);
+}));
 
+//Operator keys event listeners
 operators.forEach(button => button.addEventListener('click', (e) => {
-    numbers.push(display.textContent)
-    operator.push(e.target.textContent)
-    display.textContent = '';
-    console.log(numbers, operator)
-    if(e.target.textContent === '=') {
-      display.textContent = calculate(numbers[0], numbers[1], operator[0]);
-      numbers = []
-      operator = []
+    let op = e.target.textContent;
+    if(operator === '') {
+        num1 = displayValue;
+        operator = op;
+        displayValue = '0'
+    } else if (op === equals) {
+        num2 = displayValue;
+        let result = calculate(num1, num2, operator).toString();
+        display.textContent = result;
+        num1 = result;
+        displayValue = result;
+        operator = '';
+        num2 = '';
+     }
+}));
+
+//All-clear key
+const acKey = document.querySelector('.clear');
+acKey.addEventListener('click', () => {
+    displayValue = '0';
+    display.textContent = 0;
+    num1 = '';
+    num2 = '';
+    operator = '';
+    console.clear()
+})
+
+//Function to populate the display with numbers
+function appendNum(number) {
+    if(displayValue === '0') {
+        displayValue = number;
+        display.textContent = displayValue;
+    }else if (displayValue !== '0') {
+        displayValue = displayValue+number;
+        display.textContent = displayValue;
     }
-    console.log(numbers)
-}))
+}
+
+
+
+//When inputs are set, the next operator calculates the result & starts new operation
+//When result is displayed, disable input/stop appending numbers
