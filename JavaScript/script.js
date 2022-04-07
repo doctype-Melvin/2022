a1b5fbc709e92d99ac31964016cca52a03b27b3d
doctype-Melvin <doctype.melvin@gmail.com>
2022-04-07 11:13:36 +0200

    now adds books from form to array

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,21 @@
+//Library Cards
+let card = document.querySelector('.card');
+let title = document.querySelector('.title');
+let author = document.querySelector('.author');
+let pages = document.querySelector('.page');
+let read = document.querySelector('#read')
+
+//Input Form
+let bookTitle = document.getElementById('title');
+let bookAuthor = document.getElementById('author');
+let bookPages = document.getElementById('pages')
+let button = document.querySelector('.add');
+let inputs = document.querySelectorAll('.input')
+
+//Library array
 let theLibrary = [];
 
+//Book prototype
 function Book(title, author, pages) {
     this.title = title,
     this.author = author,
@@ -8,3 +24,15 @@ function Book(title, author, pages) {
         return `${title} written by ${author}, ${pages} pages`
     }
 }
+
+//Function to add new books to library
+function addToLibrary(){
+    let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
+    theLibrary.push(newBook);
+    return theLibrary
+}
+button.addEventListener('click', addToLibrary)
+
+
+
+
