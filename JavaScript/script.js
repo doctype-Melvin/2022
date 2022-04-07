2672caa0bca7e7b453b79fe964ea4db4f1056844
doctype-Melvin <doctype.melvin@gmail.com>
2022-04-07 09:23:12 +0200

    initial code for library project

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,10 @@
-function Book(title, author, pages, status) {
+let theLibrary = [];
+
+function Book(title, author, pages) {
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.status = status,
     this.info = function() {
-        return `${title} written by ${author}, ${pages} pages, ${status}`
+        return `${title} written by ${author}, ${pages} pages`
     }
 }
-
-const book1 = new Book('Rich Dad, Poor Dad', 'Robert Kiyosaki', 433, 'wanna read it again')
-console.log(book1.info())
